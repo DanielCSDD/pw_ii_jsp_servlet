2a02709634f89859e313123059a780cd0eabde00
Daniel Coelho <daniel.coelho.santos@gmail.com>
2024-10-26 22:06:23 -0300

    Commits - Refatoracao Pacote Model - Release-02
    Foi necessário refatorar as classes do pacote controller, pois os comentários em relação aos métodos não haviam sido aplicados.

--- src/main/java/br/com/projeto/controller/ContatoServlet.java
+++ src/main/java/br/com/projeto/controller/ContatoServlet.java
@@ -19,7 +19,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ContatoServlet
+ * A servlet {@Link ContatoServlet} receberá requisições HTTP relativo a operações de request e response.
+ * 
+ * @author Daniel Luiz Coelho dos Santos.
+ * @since 26/10-2024.
  */
 @WebServlet("/ContatoServlet")
 public class ContatoServlet extends HttpServlet {
@@ -27,13 +30,13 @@ public class ContatoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	
-	/** Atributos da classe. 
-	 **/
+	// Declaração de atributos da classe.
 	private Contato contato;
 	private ContatoService service;
 	
-	/**
-	 * Instantiates a new controller.
+	/*
+	 * Contrutor da servlet. Será onde iremos instanciar outras classes. Isso
+	 * ocorrerá quando a classe ContatoDAO for instanciado.
 	 */
 	public ContatoServlet() {
 		this.contato = new Contato();
@@ -41,9 +44,15 @@ public class ContatoServlet extends HttpServlet {
 	}
 
 	/**
-	 * Método doGet.
-	 * Receberá conexão HTTP.
+	 * O método doGet em uma servlet Java é responsável por processar solicitações HTTP GET. 
+	 * Ele permite que uma aplicação web receba e responda a dados enviados pelo navegador 
+	 * do usuário através do protocolo HTTP GET, geralmente para recuperar informações de 
+	 * um servidor. Dentro deste método, você pode ler parâmetros enviados na URL, gerar 
+	 * respostas HTML, e muito mais. É essencial para a criação de páginas web dinâmicas e 
+	 * interativas.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
@@ -52,34 +61,37 @@ public class ContatoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getServletPath();
 		
-		if (action.equals("/main")) {
-			contatos(request, response);
-		} else if (action.equals("/novo")) {
-			redirectNovoContato(request, response);
-		} else if (action.equals("/insert")) {
-			adicionarContato(request, response);
-		} else if (action.equals("/select")) {
-			listarContato(request, response);
-		} else if (action.equals("/update")) {
-			editarContato(request, response);
-		} else if (action.equals("/delete")) {
-			removerContato(request, response);
-		} else if (action.equals("/report")) {
-			gerarRelatorio(request, response);
+		// Recursos de navegação entre as páginas da aplicação.
+		if (action.equals("/viewagenda")) {
+			redirectViewAgenda(request, response);
+		} else if (action.equals("/viewnovo")) {
+			redirectViewNovoContato(request, response);
+		} else if (action.equals("/inserir")) {
+			inserir(request, response);
+		} else if (action.equals("/vieweditar")) {
+			redirectViewEditarCadastro(request, response);
+		} else if (action.equals("/atualizar")) {
+			atualizar(request, response);
+		} else if (action.equals("/excluir")) {
+			remover(request, response);
+		} else if (action.equals("/impressao")) {
+			imprimir(request, response);
 		} else {
 			response.sendRedirect("index.html");
 		}
 	}
 	
 	/**
-	 * Método que receberá uma solicitação HTTP, onde ao final da execução adicionará um contato.
+	 * Método que ao ser solicitado um novo cadastro, irá redirecionar a página .jsp responsável.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	protected void redirectNovoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void redirectViewNovoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		RequestDispatcher rd = request.getRequestDispatcher("pages/cadastro/novo.jsp");
 		
@@ -87,14 +99,17 @@ public class ContatoServlet extends HttpServlet {
 	}
 	
 	/**
-	 * Método que receberá uma solicitação HTTP, onde ao final da execução adicionará um contato.
+	 * Método que após o evento para salvar os dados do formulário (novo cadastro), irá salvar no Banco de dado, 
+	 * e em seguida será redirecionado para tela de relação dos contatos.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	protected void adicionarContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void inserir(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		contato.setNome(request.getParameter("nome"));
 		contato.setTelefone(request.getParameter("telefone"));
@@ -102,18 +117,20 @@ public class ContatoServlet extends HttpServlet {
 		
 		this.service.inserirContato(contato);
 		
-		response.sendRedirect("main");
+		response.sendRedirect("viewagenda");
 	}
 
 	/**
-	 * Método que receberá uma solicitação HTTP, onde ao final da execução listará contatos.
+	 * Método que devolverá uma lista de contatos, e em seguida será redirecionado para tela de relação dos contatos.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void redirectViewAgenda(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		ArrayList<Contato> lista = this.service.listarContatos();
 		request.setAttribute("contatos", lista);
@@ -124,14 +141,16 @@ public class ContatoServlet extends HttpServlet {
 	}
 	
 	/**
-	 * Método que receberá uma solicitação HTTP, onde ao final da execução listará um único contato.
+	 * Método que devolverá um único contato, e em seguida será redirecionado para tela de edição.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	protected void listarContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void redirectViewEditarCadastro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		contato.setId(Long.valueOf(request.getParameter("id")));
 		this.service.selecionarContato(contato);
@@ -147,14 +166,16 @@ public class ContatoServlet extends HttpServlet {
 	}
 
 	/**
-	 * Método que receberá uma solicitação HTTP, onde ao final da execução irá atualizar um contato.
+	 * Método que salvará as atualizações do contato, e em seguida será redirecionado para tela de relação dos contatos.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	protected void editarContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void atualizar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		contato.setId(Long.valueOf(request.getParameter("id")));
 		contato.setNome(request.getParameter("nome"));
@@ -163,36 +184,40 @@ public class ContatoServlet extends HttpServlet {
 		
 		this.service.alterarContato(contato);
 		
-		response.sendRedirect("main");
+		response.sendRedirect("viewagenda");
 	}
 
 	/**
-	 * Método que receberá uma solicitação HTTP, onde ao final da execução irá ecluir um contato.
+	 * Método que excluirá um contato, e em seguida será redirecionado para tela de relação dos contatos.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	protected void removerContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void remover(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String idString = request.getParameter("id");
 		contato.setId(Long.valueOf(idString));
 		
 		this.service.deletarContato(contato);
 		
-		response.sendRedirect("main");
+		response.sendRedirect("viewagenda");
 	}
 
 	/**
-	 * Método que receberá uma solicitação HTTP, onde ao final da execução irá gerar um PDF contendo os contatos.
+	 * Método que irá gerar um arquivo em PDF contendo os contatos do Banco de Dados.
 	 *
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
 	 * @param request the request
 	 * @param response the response
 	 * @throws ServletException the servlet exception
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	protected void gerarRelatorio(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void imprimir(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Document documento = new Document();
 		try {
 			response.setContentType("apllication/pdf");
--- src/main/java/br/com/projeto/controller/service/ContatoService.java
+++ src/main/java/br/com/projeto/controller/service/ContatoService.java
@@ -5,22 +5,30 @@ import java.util.ArrayList;
 import br.com.projeto.model.Contato;
 import br.com.projeto.model.dao.ContatoDAO;
 
+/**
+ * A classe {@code ContatoService} é uma classe que irá prover recursos de chamada ao Banco de Dados.
+ * Bem, como irá implementar regras de negócio.
+ * 
+ * @author Daniel Luiz Coelho dos Santos.
+ * @since 26/10-2024.
+ */
 public class ContatoService {
 	
-	/** 
-	 * Instanciando a classe DAO. 
-	 * */
+	// Declaração de atributos da classe.
 	private ContatoDAO dao;
 	
-	/** 
-	 * Construtor da classe service. 
+	/*
+	 * Contrutor da classe. Será onde iremos instanciar outras classes. Isso
+	 * ocorrerá quando a classe ContatoDAO for instanciado.
 	 */
 	public ContatoService() {
 		this.dao = new ContatoDAO();
 	}
 
 	/**
-	 * Chamando o método da classe DAO, que irá gravar o registro no Banco de Dados.
+	 * Chamando o método da classe {@link ContatoDAO}, que irá gravar o registro no Banco de Dados.
+	 *
+	 * @author Daniel Luiz Coelho dos Santos.
 	 *
 	 * @param contato
 	 */
@@ -30,36 +38,43 @@ public class ContatoService {
 	
 
 	/**
-	 * Chamando o método da classe DAO, que irá listar os registros do Banco de Dados.
+	 * Chamando o método da classe {@link ContatoDAO}, que irá listar os registros do Banco de Dados.
 	 *
-	 * @return ArrayList<Contato>
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
+	 * @return {@code ArrayList}
 	 */
 	public ArrayList<Contato> listarContatos() {
 		return this.dao.findAll();
 	}
 	
 	/**
-	 * Chamando o método da classe DAO, que irá selecionar um registro do Banco de Dados.
-	 *
-	 * @return Contato contato
+	 * Chamando o método da classe {@link ContatoDAO}, que irá selecionar um registro do Banco de Dados.
+	 * 
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
+	 * @param {@code Contato}
+	 * @return {@code Contato}
 	 */
 	public Contato selecionarContato(Contato contato) {
 		return this.dao.findById(contato);
 	}
 	
 	/**
-	 * Chamando o método da classe DAO, que irá atualizar um registro do Banco de Dados.
+	 * Chamando o método da classe {@link ContatoDAO}, que irá atualizar um registro do Banco de Dados.
 	 *
-	 * @return Contato contato
+	 * @author Daniel Luiz Coelho dos Santos.
+	 * 
+	 * @param {@code Contato}
 	 */
 	public void alterarContato(Contato contato) {
 		this.dao.update(contato);
 	}
 	
 	/**
-	 * Chamando o método da classe DAO, que irá excluir um registro do Banco de Dados.
+	 * Chamando o método da classe {@link ContatoDAO}, que irá excluir um registro do Banco de Dados.
 	 *
-	 * @return Contato contato
+	 * @param {@code Contato}
 	 */
 	public void deletarContato(Contato contato) {
 		this.dao.delete(contato);
